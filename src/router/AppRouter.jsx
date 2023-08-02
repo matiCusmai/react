5de8ad338b761cdc3cae4f601f983b2e1d9ccf53
Matias Cusmai <maticusmai@gmail.com>
2023-08-02 11:45:38 -0300

    building

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/header/header"
 import ItemListContainer from "../components/ItemListContainer/ItemListContainer"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Nosotros from "../components/nosotros/nosotros"
+import Nosotros from "../components/nosotros/Nosotros"
 import Contacto from "../components/Contacto/Contacto"
 import ItemDetailContainer from "../components/ItemDetailContainer/ItemDetailContainer"
 import Cart from "../components/Cart/Cart"
